f486ed6662c984bc6307a964b36fa0fa4c140321
katherinesullivan <kes.sullivan1@gmail.com>
2020-10-19 17:11:48 -0300

    4s

--- plancha2/3a1.s
+++ plancha2/3a1.s
@@ -1,6 +1,6 @@
 .global main
 main:
-    # 1 << 31
+    #1 << 31
     movl $1, %eax
     shll $0x1f, %eax
     ret
--- plancha2/4.s
+++ plancha2/4.s
@@ -1,10 +1,13 @@
 .global main
 main:
-    movl $-1, %eax # Solo para este tama~no el mov pone en 0
+    mov $-1, %rax # Solo para este tama~no el mov pone en 0
                   # la parte alta del registro.
     movl $2, %ecx
     imull %ecx
-    #completar para que el resultado correcto como signed quede en rax 
+    #completar para que el resultado correcto como signed quede en rax
+    movl $-1, %ebx
+    shlq $32, %rbx
+    orq %rbx, %rax
     
 
     xorq %rax, %rax
@@ -13,6 +16,7 @@ main:
     mulw %cx
     #completar para que el resultado correcto como unsigned short
     #quede en eax
-    ...
-    ...
+    shll $16, %edx
+    orl %edx, %eax
+
     ret
