31d1c96f08bda5031a441b20608969f22def5d97
inescipullo <64146923+inescipullo@users.noreply.github.com>
2020-10-19 17:57:23 -0300

    ej 1

--- plancha2/1a.c
+++ plancha2/1a.c
@@ -1,3 +1,13 @@
+/*
+Código en ASM:
+.global main
+main:
+    movb $0xFF, %al
+    ret
+*/
+// Devuelve 255 como código de salida.
+
+
 int main (){
     return 255;
 }
--- plancha2/1b.c
+++ plancha2/1b.c
@@ -1,3 +1,15 @@
+/*
+Codigo en ASM:
+.global main
+main:
+    movb $0xFE, %al 
+    movb $-1, %bl 
+    addb %bl, %al 
+    incb %bl
+    ret
+*/
+// Devuelve 253 como código de salida.
+
 char main(){
     char a, b;
     a = 254;
