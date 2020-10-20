f7ed9ef040647364f0a3f83dc065e6cb54e28c76
katherinesullivan <kes.sullivan1@gmail.com>
2020-10-20 15:55:12 -0300

    xd

--- plancha2/6.s
+++ plancha2/6.s
@@ -1,7 +1,8 @@
-.data
+    .data
 format: .asciz "%ld\n"
 i: .quad 0xDEADBEEF
-.text .global main
+    .text 
+    .global main
 main:
     movq $format, %rdi  # El primer argumento es el formato. 
     movq $1234, %rsi  # El valor a imprimir.
@@ -18,10 +19,10 @@ main:
 
 
 
-a) El valor del registro rsp.
-b) La direcci ́on de la cadena de formato.
-c) La direcci ́on de la cadena de formato en hexadecimal. 
-d) El quad en el tope de la pila.
-e) El quad ubicado en la direcci ́on rsp + 8. 
-f) El valor i.
-g) La direcci ́on de i.
+#a) El valor del registro rsp.
+#b) La direcci ́on de la cadena de formato.
+#c) La direcci ́on de la cadena de formato en hexadecimal. 
+#d) El quad en el tope de la pila.
+#e) El quad ubicado en la direcci ́on rsp + 8. 
+#f) El valor i.
+#g) La direcci ́on de i.
