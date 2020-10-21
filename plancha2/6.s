407985bd57d633025afc0c921299e32525ef757a
katherinesullivan <kes.sullivan1@gmail.com>
2020-10-21 10:34:51 -0300

    cx

--- plancha2/6.s
+++ plancha2/6.s
@@ -1,5 +1,5 @@
     .data
-format: .asciz "%ld\n"
+formato: .asciz "%ld\n"
 format2: .asciz "%lx\n"
 i: .quad 0xDEADBEEF
 
@@ -7,49 +7,49 @@ i: .quad 0xDEADBEEF
     .global main
 # FALTA VER QUE B Y C ESTEN BIEN (COINCIDEN // SI) Y COMO HACER LO DE +8 EN E
 main:
-    movq $format, %rdi  # El primer argumento es el formato. 
+    movq $formato, %rdi  # El primer argumento es el formato. 
     movq $1234, %rsi  # El valor a imprimir.
     xorq %rax, %rax  # Cantidad de valores de punto flotante. 
     call printf
 
     # apartado a
-    movq $format, %rdi
+    movq $formato, %rdi
     movq %rsp, %rsi
     xorq %rax, %rax
     call printf
 
     # apartado b
-    movq $format, %rdi
-    movq $format, %rsi
+    movq $formato, %rdi
+    movq $formato, %rsi
     xorq %rax, %rax
     call printf
 
     # apartado c
     movq $format2, %rdi
-    movq $format, %rsi
+    movq $formato, %rsi
     xorq %rax, %rax
     call printf
 
     # apartado d
-    movq $format, %rdi
+    movq $formato, %rdi
     movq %rsp, %rsi
     xorq %rax, %rax
     call printf
 
     # apartado e
-    movq $format, %rdi
-    movq 8( ,%rsp,1), %rsi
+    movq $formato, %rdi
+    leaq 8(%rsp), %rsi
     xorq %rax, %rax
     call printf
 
     # apartado f
-    movq $format, %rdi
+    movq $formato, %rdi
     movq i, %rsi
     xorq %rax, %rax
     call printf
 
     # apartado g
-    movq $format, %rdi
+    movq $formato, %rdi
     movq $i, %rsi
     xorq %rax, %rax
     call printf
