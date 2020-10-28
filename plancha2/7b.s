097d23a70aaa29b222a2cb048253415706824c12
inescipullo <64146923+inescipullo@users.noreply.github.com>
2020-10-28 17:32:11 -0300

    ..

--- plancha2/7b.s
+++ plancha2/7b.s
@@ -1,6 +1,14 @@
-.text
-.global main
+    .data
+formato: .asciz "%d\n"
+
+    .text
+    .global main
 main:
+    movq formato, %rdi
+    movq %rax, %rsi
+    xor %rax, %rax
+    call printf
+    movq %rsi, %rax
     movq $0, %rcx
     movq $64, %rbx
 
