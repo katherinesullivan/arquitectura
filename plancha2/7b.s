91e8bd89de83ca79ea46945a62d2a58194a90fbd
inescipullo <64146923+inescipullo@users.noreply.github.com>
2020-10-28 17:14:58 -0300

    ej 7b

--- plancha2/7b.s
+++ plancha2/7b.s
@@ -1,3 +1,22 @@
+.text
 .global main
 main:
-    
+    movq $0, %rcx
+    movq $64, %rbx
+
+condicion:
+    decq %rbx
+    jnz while
+    jz retorno
+
+while: 
+    rol $1, %rax
+    jc suma
+    jmp condicion
+
+suma:
+    addq %rcx
+    jmp comdicion
+
+retorno:
+    ret
