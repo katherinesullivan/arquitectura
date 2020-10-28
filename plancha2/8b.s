c2ee7a7d8d79dfb5fd8aea6fea8a7f6b403f22f6
inescipullo <64146923+inescipullo@users.noreply.github.com>
2020-10-28 19:39:29 -0300

    8b

---
+++ plancha2/8b.s
@@ -0,0 +1,30 @@
+    .data
+cadenalarga: .asciz "123456"
+cadenacorta: .asciz "123" #=>1 , 124=>0, 456=>0
+    .text
+    .global main 
+main:
+    movq $cadenalarga, %rdi
+    movq $cadenacorta, %rsi
+    movl $3, %edx  #debe ser la longitud de cadena corta
+
+    xorq %rax, %rax
+    incq %edx
+
+    .global compara 
+compara:
+    #retorna 1 si las cadenas son iguales en los primeros %edx caracteres
+    #retorna 0 si son distintas
+    decl %edx
+    jnz soniguales
+    cmpb %rdi, %rsi
+    jnz retorno
+    incq %rdi
+    incq %rsi
+    jmp compara
+
+soniguales:
+    incq %rax
+
+retorno:
+    ret
