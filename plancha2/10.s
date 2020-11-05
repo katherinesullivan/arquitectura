49d36309441c727d268ea493df41d9e51ed9023c
inescipullo <64146923+inescipullo@users.noreply.github.com>
2020-11-05 19:12:51 -0300

    ,,

--- plancha2/10.s
+++ plancha2/10.s
@@ -1,15 +1,44 @@
     .data
 valor: .long 10
-    .text
-    .global main
-main:
-    xorq %rdx, %rdx  # rdx es value
+formato: .asciz "%ld\n"
+first: .quad 0x1
+second: .quad 0x2
 
+
+    .text
 setjmp2:
-    movq setjmp2, %rcx  # rcx es el buffer
+    popq %rcx  # rcx es el buffer
     movq %rdx, %rax
+
 
 longjmp2:
     movq valor, %rdx
     jmp *%rcx
+
+first:
+    jmp second
+    # Estas instrucciones nunca se llegan a ejecutar
+    movq $formato, %rdi
+    movq first, %rsi
+    xorq %rax, %rax
+    call printf
     
+second:
+    movq $formato, %rdi
+    movq second, %rsi
+    xorq %rax, %rax
+    call printf   # Esto si se imprime
+    jmp longjmp2
+
+
+
+    .global main
+main:
+    xorq %rdx, %rdx  # rdx es value
+setjmp:
+    push setjmp
+    call setjmp2
+    cmpq $0, $rax
+    jz first
+    
+    ret    
