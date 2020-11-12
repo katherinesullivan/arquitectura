f556096dd0f68611b124bd971c302c2f821a1f1f
katherinesullivan <kes.sullivan1@gmail.com>
2020-11-12 17:38:14 -0300

    a

--- plancha2/10.s
+++ plancha2/10.s
@@ -1,5 +1,5 @@
     .data
-buffer: .jmp_buffer 0, 0, 0, 0, 0, 0, 0, 0
+buffer: .quad 0, 0, 0, 0, 0, 0, 0, 0
 valor: .long 10
 formato: .asciz "%ld\n"
 first: .quad 0x1
@@ -9,22 +9,29 @@ second: .quad 0x2
     .text
 setjmp2:
     movq %rbx, buffer
-    movq %rbp, 8(buffer)
-    movq %rsp, 16(buffer)
-    movq %r12, 24(buffer)
-    movq %r13, 32(buffer)
-    movq %r14, 40(buffer)
-    movq %r15, 48(buffer)
-    movq (%rsp), 56(buffer)
-    
-
-    movq 8(%rbp), %rcx  # rcx es el buffer
+    movq %rbp, buffer+1
+    movq %rsp, buffer+2
+    movq %r12, buffer+3
+    movq %r13, buffer+4
+    movq %r14, buffer+5
+    movq %r15, buffer+6
+    movq (%rsp), buffer+7
+    xor %rdx, %rdx
     movq %rdx, %rax
 
+retornosjdelj:
+    cmpq $0, %rdx
+    jz rdxmas1
+    movq %rdx, %rax
+    jmp *56(buffer)
+
+rdxmas1:
+    incq %rdx
+    jmp retornosjdelj
 
 longjmp2:
-    movq valor, %rdx
-    jmp *%rcx
+    movq $1, %rdx
+    jmp retornosjdelj
 
 primero:
     jmp segundo
@@ -45,22 +52,38 @@ segundo:
 
     .global main
 main:
-    # Prologo
-    pushq %rbp
-    movq  %rsp, %rbp
-    
-    
-    xorq %rdx, %rdx  # rdx es value
-setjmp:
-    push setjmp
     call setjmp2
-    
-    addq $8, (%rbp)
     cmpq $0, %rax
     jz primero
+    ret
+
+
+
+
+
+
+
+
+
+
+
+
+    # Prologo
+    #pushq %rbp
+    #movq  %rsp, %rbp
+    
+    
+    #xorq %rdx, %rdx  # rdx es value
+#setjmp:
+#    push setjmp
+#    call setjmp2
+    
+ #   addq $8, (%rbp)
+  #  cmpq $0, %rax
+   # jz primero
     
 
     # Epilogo
-    movq %rbp, %rsp
-    popq %rbp
-    ret    
+    #movq %rbp, %rsp
+    #popq %rbp
+    #ret    
