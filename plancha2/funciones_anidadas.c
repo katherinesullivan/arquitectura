375daddc97dda13813a39123e68d944f9c5ed42b
inescipullo <64146923+inescipullo@users.noreply.github.com>
2020-11-18 16:37:50 -0300

    ..

---
+++ plancha2/funciones_anidadas.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int p1() {
+    int a = 5;
+    int p2() {
+        int b = a+1;
+        int p3() {
+            int c = a * b;
+            return c;
+        }
+        return p3();
+    }
+    return p2();
+}
+
+int main() {
+    printf("%d\n", p1());
+    return 0;
+}
