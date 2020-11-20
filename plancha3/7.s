30cf0bcae5698ce4a492ab8782364b0baf95e3b4
inescipullo <64146923+inescipullo@users.noreply.github.com>
2020-11-20 15:47:34 -0300

    ..

--- plancha3/7.s
+++ plancha3/7.s
@@ -1,6 +1,16 @@
+    .data
+a: .float 0, 1, 2, 3
+b: .float 4, 5, 6, 7
+len: .long 4
+i: .long 0
+    
     .text
     .global main
 main:
+    movq $a, %rdi
+    movq $b, %rsi
+    movq len, %rdx
+    incq %rdx
     call sum
     ret
 
@@ -13,11 +23,13 @@ sum:
     jz retorno
     # algo corte direccionamiento relativo para ir accediendo
     # a todos los elementos del array porque creo que esto solo accederia al primero 
-    movss (%rdi), %xmm0 # esta en el apunte3 (con funciones packed) pero o sea deberia funcar y no lo hace
-    movss (%rsi), %xmm1
+
+    movss (%rdi, i, 4), %xmm0 # esta en el apunte3 (con funciones packed) pero o sea deberia funcar y no lo hace
+    movss (%rsi, i, 4), %xmm1
     addss %xmm0, %xmm1
     # guarda el resultado en "a"
-    movss %xmm1, (%rdi)
+    movss %xmm1, (%rdi, i, 4)
+    incl i
     jmp sum
 
 retorno:
--- plancha3/8.s
+++ plancha3/8.s
@@ -1,16 +1,57 @@
-.global sum
-sum:
+    .data
+a: .float 0, 1, 2, 3
+b: .float 4, 5, 6, 7
+len: .quad 4
+i: .long 0 # creo que para los contadores habria que usar registros
+j: .long 0
+    
+    .text
+    .global main
+main:
+    movq $a, %rdi
+    movq $b, %rsi
+    movq len, %rdx
+    incq %rdx
+    call sum
+    ret
+
+    
+    .global sum_simd
+sum_simd:
     # notar que en este caso los argumentos
     # son punteros y vienen en rdi y rsi respectivamente
+    cmpq $4, %rdx
+    jle sum
 
-    # copia los 4 floats de "a" a xmm0
-    movaps (%rdi), %xmm0
-    # copia los 4 floats de "b" a xmm1
-    movaps (%rsi), %xmm1
+    mull $16, j
+
+    # copia 4 floats de "a" a xmm0
+    movaps j(%rdi), %xmm0
+    # copia 4 floats de "b" a xmm1
+    movaps j(%rsi), %xmm1
     # suma los 4 floats a la vez
     addps %xmm0, %xmm1
     # guarda el resultado en "a"
-    movaps %xmm1, (%rdi)
-    ret
+    movaps %xmm1, j(%rdi)
+    
+    incq j
+    subq $4, %rdx
+    jz retorno
+    jmp sum_simd
 
-# repetir esto
+
+sum:
+    decq %rdx
+    jz retorno
+
+    movss (%rdi, i, 4), %xmm0 # esta en el apunte3 (con funciones packed) pero o sea deberia funcar y no lo hace
+    movss (%rsi, i, 4), %xmm1
+    addss %xmm0, %xmm1
+
+    movss %xmm1, (%rdi, i, 4)
+    incl i
+    jmp sum
+
+
+retorno:
+    ret
