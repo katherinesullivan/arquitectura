f83f8b2bbed44fc381d0d82cb439f57de7654eec
inescipullo <64146923+inescipullo@users.noreply.github.com>
2020-11-20 16:57:42 -0300

    ..

---
+++ plancha3/8.c
@@ -0,0 +1,20 @@
+
+void sum_simd(float *, float *, int);
+
+int main() {
+    int len = 4;
+    float *a = malloc(sizeof(float) * len);
+    float *b = malloc(sizeof(float) * len);
+    for (int i = 0; i < len; i++) {
+        a[i] = i + (i/10);
+        b[i] = i - (i/10);
+    }
+
+    sum_simd(a, b, len);
+
+    for (int i = 0; i < len; i++) {
+        printf("%f ", a[i]);
+    }
+    puts("");
+    return 0;
+}
