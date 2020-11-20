b1c62df96b4ecfb64acfdf0a3c414d4dded4d84d
katherinesullivan <kes.sullivan1@gmail.com>
2020-11-20 17:09:18 -0300

    a

--- plancha3/8.c
+++ plancha3/8.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void sum_simd(float *, float *, int);
 
@@ -10,10 +11,10 @@ int main() {
         a[i] = i + (i/10);
         b[i] = i - (i/10);
     }
-
+    len++;
     sum_simd(a, b, len);
 
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len-1; i++) {
         printf("%f ", a[i]);
     }
     puts("");
