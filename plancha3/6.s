8693ddd9d620e447227758b6503c196947f7c765
katherinesullivan <kes.sullivan1@gmail.com>
2020-11-20 15:46:04 -0300

    hola

--- plancha3/6.s
+++ plancha3/6.s
@@ -1,9 +1,9 @@
     .data
 zero: .long 0
 a: .float 0
-b: .float 0
+b: .float 2
 c: .float 20
-d: .float 0
+d: .float 1
 e: .float 2
 f: .float 4
 var1: .float 1.00000
@@ -11,7 +11,7 @@ var2: .float 1.00000
 formato: .asciz "%f\n"
 
     .text
-    .global solve
+    .global eliminacionprogresiva
 # xmm0 = a, xmm1 = b, xmm2 = c, xmm3 = d, xmm4 = e, xmm5 = f
 
 a0:
@@ -20,27 +20,12 @@ a0:
     ucomiss %xmm6, %xmm1 # si b=0 tendremos infintas sol (c=0) o ninguna (c!=0)
     jz incompoinf
     divss %xmm1, %xmm2 # en %xmm2 queda el valor de y
-    movss %xmm2, var2 # se carga el contenido de %xmm2 en la dir de memoria de var2
-    mulss %xmm2, %xmm4
-    subss %xmm4, %xmm5
+    movss %xmm2, (%rsi) # se carga el contenido de %xmm2 en la dir de memoria de var2
+    mulss %xmm2, %xmm4 # xmm4 = ey
+    subss %xmm4, %xmm5 # xmm5 = f-ey
     divss %xmm3, %xmm5 # queda en %xmm5 el valor de x
-    movss %xmm5, var1 # se carga el contenido de %xmm5 en la dir de memoria de var1
+    movss %xmm5, (%rdi) # se carga el contenido de %xmm5 en la dir de memoria de var1
     jmp retornonormal
-
-    .global main
-main:
-    movss a, %xmm0
-    movss b, %xmm1
-    movss c, %xmm2
-    movss d, %xmm3
-    movss e, %xmm4
-    movss f, %xmm5
-    movq $0, %rax
-    cvtsi2ssq %rax, %xmm6
-    #movq $var1, %rdi
-    #movq $var2, %rsi
-    call eliminacionprogresiva
-    ret
     
 retornonormal:
     movq $0, %rax
@@ -60,10 +45,24 @@ eliminacionprogresiva:
     ucomiss %xmm6, %xmm5 # si f-(d/a) = 0 entonces el sistema tendrá infintas sol o ninguna
     jz incompoinf
     divss %xmm4, %xmm5 # en xmm5 queda el valor de y
-    movss %xmm5, var2 # se carga el contenido de xmm5 en la dir de memoria de var2
+    movss %xmm5, (%rsi) # se carga el contenido de xmm5 en la dir de memoria de var2
     mulss %xmm5, %xmm1
     subss %xmm1, %xmm2
     divss %xmm0, %xmm2 # en xmm2 queda el valor de x
-    movss %xmm2, var1 # se carga el contenido de xmm2 en la dir de memoria de var1
+    movss %xmm2, (%rdi) # se carga el contenido de xmm2 en la dir de memoria de var1
     jmp retornonormal
 
+    .global main
+main:
+    movss a, %xmm0
+    movss b, %xmm1
+    movss c, %xmm2
+    movss d, %xmm3
+    movss e, %xmm4
+    movss f, %xmm5
+    movq $0, %rax
+    cvtsi2ssq %rax, %xmm6
+    movq $var1, %rdi
+    movq $var2, %rsi
+    call eliminacionprogresiva
+    ret
