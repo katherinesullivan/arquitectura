6b38e1cb6a96e15b548ad9666d3ca19f7912c791
inescipullo <64146923+inescipullo@users.noreply.github.com>
2020-11-24 17:38:54 -0300

    //

--- plancha3/1.c
+++ plancha3/1.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-//#include <ieee754.h>
+// #include <ieee754.h>
 
 
 #define EXP(x) ((*(int *) (&x) >> 23) & 0xff)
 #define FRAC(x) (*(int *) (&x) & 0x7fffff)
+#define SGN(x) ((*(int *) (&x) >> 31) & 0x1)
+
 
 int main() {
-    float f = 3.14;
+    float f = -3.14;
+    printf("%x\n", SGN(f));
     printf("%x\n", EXP(f));
     printf("%x\n", FRAC(f));
     return 0;
