7000d8e2677f37c0d385edbdc6a8cb21ae9e94fb
katherinesullivan <kes.sullivan1@gmail.com>
2020-11-24 18:01:23 -0300

    a

--- plancha3/1.c
+++ plancha3/1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-// #include <ieee754.h>
+#include <ieee754.h>
 
 
 #define EXP(x) ((*(int *) (&x) >> 23) & 0xff)
@@ -9,8 +9,13 @@
 
 int main() {
     float f = -3.14;
+    union ieee754_float u;
+    u.f = f;
     printf("%x\n", SGN(f));
+    printf("%x\n", u.ieee.negative);
     printf("%x\n", EXP(f));
+    printf("%x\n", u.ieee.exponent);
     printf("%x\n", FRAC(f));
+    printf("%x\n", u.ieee.mantissa);
     return 0;
 }
