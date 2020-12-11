a4b583726663e023fec1692152fe2840c8c47cb5
katherinesullivan <kes.sullivan1@gmail.com>
2020-12-11 12:27:38 -0300

    a

--- plancha4/ejercicio4.s
+++ plancha4/ejercicio4.s
@@ -1,25 +1,21 @@
-    .section .rodata
-i: .word 2
-j: .word 3
-
     .text
     .arm
     .global main
 
 main:
-    ldr r0, =i
-    ldr r1, =j
+    ldr r0, =0xd
+    ldr r1, =0xb
     mov r2, #0
-
 while:
     and r3, r1, #1
     cmp r3, #1
     addeq r2, r2, r0
     subeq r1, r1, #1
-    movne r0, r0, LSL #2
-    movne r1, r1, LSR #2
+    movne r0, r0, LSL #1
+    movne r1, r1, LSR #1
     cmp r1, #1
-    bge while
+    bhs while
+    
     mov r0, r2
     bx lr
 
