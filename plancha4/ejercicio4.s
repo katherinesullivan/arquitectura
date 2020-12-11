aac2d8a967cbce74382e574134f9ea306d1c69b7
katherinesullivan <kes.sullivan1@gmail.com>
2020-12-11 11:50:23 -0300

    a

--- plancha4/ejercicio4.s
+++ plancha4/ejercicio4.s
@@ -11,12 +11,6 @@ main:
     ldr r1, =j
     mov r2, #0
 
-    cmp r1, #1
-    bhi while
-    add r2, r2, r0
-    mov r0, r2
-    bx ld
-
 while:
     and r3, r1, #1
     cmp r3, #1
@@ -24,5 +18,8 @@ while:
     subeq r1, r1, #1
     movne r0, r0, LSL #2
     movne r1, r1, LSR #2
-    
+    cmp r1, #1
+    bge while
+    mov r0, r2
+    bx lr
 
